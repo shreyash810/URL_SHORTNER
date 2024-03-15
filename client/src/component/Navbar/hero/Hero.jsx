981574f109145c211ade0ac13bf44aac103be777
shahid056 <shaikhs2956@gmail.com>
2024-03-15 09:49:09 +0530

    done

--- client/src/component/Navbar/hero/Hero.jsx
+++ client/src/component/Navbar/hero/Hero.jsx
@@ -23,7 +23,7 @@ function Hero() {
     };
     try {
       const res = await axios.post(
-        "/https://url-shortnerapi-opal.vercel.app/url",
+        "https://url-shortnerapi-opal.vercel.app/url",
         { url: url },
         config
       );
