341820cec5b9b0b52bf7ab9c21efcd3f6b65216a
shahid056 <107267232+shahid056@users.noreply.github.com>
2024-03-15 10:14:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieparser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://url-shortners-two.vercel.app"],
     credentials: true,
     sameSite: "none",
   })
